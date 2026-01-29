89bcaf4cab56c94fef0de4c198f3633d066abc74
German <germanmendoza03@gmail.com>
2026-01-29 15:19:52 -0300

    navbar custom

---
+++ public/components/nav-bar-custom.js
@@ -0,0 +1,23 @@
+class NavBarCustom extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <div style="margin-bottom:16px">
+        <label style="font-weight:600;margin-right:8px">Dashboard</label>
+        <select id="nav-select">
+          <option value="/facturas">General</option>
+          <option value="/productos">Ventas por producto</option>
+          <option value="/vendedores">Ranking vendedores</option>
+        </select>
+      </div>
+    `;
+
+    const select = this.querySelector('#nav-select');
+    select.value = window.location.pathname;
+
+    select.addEventListener('change', e => {
+      window.location.href = e.target.value;
+    });
+  }
+}
+
+customElements.define('nav-bar-custom', NavBarCustom);
